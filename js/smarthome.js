908681513d9c42164c13bfefdaa02122b50ca953
KabirMathur0108 <39233046+KabirMathur0108@users.noreply.github.com>
2018-11-24 03:20:17 +0530

    Add files via upload

---
+++ js/smarthome.js
@@ -0,0 +1,23 @@
+function toggleMenu(idx) {
+	var menulinks = document.getElementsByClassName("toplink");
+	for(i = 0; i < menulinks.length; i++) {
+		menulinks[i].style.borderBottom = "2px solid transparent";
+	}
+	menulinks[idx].style.borderBottom = "2px solid #6466AA";
+}
+
+setTimeout(function() {lighthome();},2500);
+
+function lighthome() {
+	var homeimg = document.getElementById("imghome");
+	var mobileimg = document.getElementById("imgmobilehand");
+	if (homeimg.src.search("homelit.png") > -1) {
+		homeimg.src = "images/homedark.png";
+		mobileimg.src = "images/mobilehandoff.png";
+		
+	}
+	else {
+		homeimg.src = "images/homelit.png";
+		mobileimg.src = "images/mobilehand.png";
+	}
+}
